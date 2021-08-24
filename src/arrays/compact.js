82814ada3bce1b0267521312d84bb613e796bd3c
Iryna Stsiarzhanava <iryna.stsiarzhanava@gmail.com>
2021-08-24 20:51:09 +0200

    fix: remove array prototype methods from compact function

--- src/arrays/compact.js
+++ src/arrays/compact.js
@@ -1,5 +1,18 @@
 function compact(array) {
-  return array.filter(Boolean);
+  const trueArr = [];
+  let falsy = false;
+  let length = 0;
+  while (array[length] !== undefined) {
+    length += 1;
+  }
+
+  for (let i = 0; i < length; i += 1) {
+    falsy = Boolean(array[i]);
+    if (falsy === true) {
+      trueArr[trueArr.length] = array[i];
+    }
+  }
+  return trueArr;
 }
 
 module.exports = compact;
