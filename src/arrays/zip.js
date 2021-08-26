387980b9421c46fd6ea5035213392d91c44c6cf4
Iryna Stsiarzhanava <iryna.stsiarzhanava@gmail.com>
2021-08-26 17:33:54 +0200

    fix: remove zip functiton

--- src/arrays/zip.js
+++
@@ -1,9 +0,0 @@
-function zip(...arrays) {
-  const minLen = Math.min(...arrays.map((arr) => arr.length));
-  const [firstArr, ...restArrs] = arrays;
-  return firstArr.slice(0, minLen).map(
-    (val, i) => [val, ...restArrs.map((arr) => arr[i])],
-  );
-}
-
-module.exports = zip;
